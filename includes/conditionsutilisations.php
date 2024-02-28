ff8e28fb234c65cea94c7086421e056c6ef94705
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2024-02-28 16:35:31 +0100

    pages

---
+++ includes/conditionsutilisations.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Raoul's Art - Home</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Chewy&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <header>
+            <?php
+            include("includes/header.php");
+            ?>
+        </header>
+
+        <main>
+            <div class="mentions chewy-regular">
+                <h2 class="center">Conditions d'Utilisation - Raoul's Art</h1>
+
+                <h2>1. Acceptation des Conditions d'Utilisation</h2>
+                <p>En accédant et en utilisant ce site web, vous acceptez pleinement et sans réserve les présentes Conditions d'Utilisation.</p>
+
+                <h2>2. Utilisation du Contenu</h2>
+                <p>Le contenu de ce site est protégé par le droit d'auteur. Toute utilisation non autorisée du contenu, y compris sa reproduction, sa modification, sa distribution ou sa republication, est strictement interdite.</p>
+
+                <h2>3. Liens Externes</h2>
+                <p>Ce site peut contenir des liens vers des sites web externes. Raoul's Art n'est pas responsable du contenu de ces sites externes.</p>
+
+                <h2>4. Limitation de Responsabilité</h2>
+                <p>Malgré tous nos efforts pour assurer l'exactitude des informations présentes sur ce site, Raoul's Art ne peut garantir l'exactitude, l'exhaustivité ou la pertinence de ces informations. En conséquence, l'utilisation des informations présentes sur ce site se fait sous votre propre responsabilité.</p>
+
+                <h2>5. Modifications des Conditions d'Utilisation</h2>
+                <p>Raoul's Art se réserve le droit de modifier à tout moment les présentes Conditions d'Utilisation. Il vous incombe de consulter régulièrement cette page pour prendre connaissance des éventuelles modifications.</p>
+
+                <h2>6. Contact</h2>
+                <p>Pour toute question concernant les Conditions d'Utilisation, veuillez contacter Raoul's Art à l'adresse suivante : <a href="mailto:contact@raoulsart.com">contact@raoulsart.com</a>.</p>
+
+                <p>Fait à Saint-Nazaire, le 28/02/2024.</p>
+            </div>
+        </main>
+
+        <footer>
+            <?php
+            include("includes/footer.php");
+            ?>
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
---
+++ mentions.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Raoul's Art - Home</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Chewy&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <header>
+            <?php
+            include("includes/header.php");
+            ?>
+        </header>
+
+        <main>
+            <div class="mentions chewy-regular">
+                <h2 class="center">Mentions Légales - Raoul's Art</h2>
+                <h2>Éditeur du site :</h2>
+                <p>Raoul's Art</p>
+                <p>Adresse : 1 boulevard de l'université</p>
+                <p>Téléphone : 06 62 81 99 37</p>
+                <p>Email : <a href="mailto:contact@raoulsart.com">felis.maillard@agfagoofay.fr</a></p>
+
+                <h2>Directeur de la publication :</h2>
+                <p>Félis Maillard</p>
+
+                <h2>Propriété intellectuelle :</h2>
+                <p>Le contenu de ce site (textes, images, vidéos, etc.) est protégé par le droit d'auteur. Toute reproduction ou utilisation sans autorisation préalable est strictement interdite.</p>
+
+                <h2>Protection des données personnelles :</h2>
+                <p>Conformément à la loi informatique et libertés du 6 janvier 1978, vous disposez d'un droit d'accès, de modification et de suppression des données vous concernant. Pour exercer ce droit, veuillez contacter <a href="mailto:felis.maillard@agfagoofay.fr">Raoul's Art</a>.</p>
+
+                <h2>Cookies :</h2>
+                <p>Ce site utilise des cookies à des fins de statistiques et d'amélioration de l'expérience utilisateur. En naviguant sur ce site, vous consentez à l'utilisation de ces cookies.</p>
+
+                <h2>Liens externes :</h2>
+                <p>Raoul's Art n'est pas responsable du contenu des sites externes vers lesquels des liens sont établis.</p>
+
+                <h2>Responsabilité :</h2>
+                <p>Raoul's Art s'efforce de fournir des informations exactes et à jour sur ce site, mais ne peut garantir leur exhaustivité ou leur exactitude. L'utilisateur utilise ce site sous sa propre responsabilité.</p>
+
+                <h2>Crédits :</h2>
+                <p>Le design et la conception de ce site ont été réalisés par Agfagoofay.</p>
+
+                <h2>Modification des mentions légales :</h2>
+                <p>Raoul's Art se réserve le droit de modifier les présentes mentions légales à tout moment. Les utilisateurs sont invités à les consulter régulièrement.</p>
+
+                <p>Fait à Saint-Nazaire, le 28/02/2024.</p>
+            </div>
+        </main>
+
+        <footer>
+            <?php
+            include("includes/footer.php");
+            ?>
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
